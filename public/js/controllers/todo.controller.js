344dad3b7a7eb34991135503f7ba5fefc9729a24
Kayleigh Yu <kayleighyu@gmail.com>
2017-03-17 20:48:45 -0400

    implemented a way for the user the click an edit button which will allow them to update their todo.

--- public/js/controllers/todo.controller.js
+++ public/js/controllers/todo.controller.js
@@ -9,6 +9,8 @@
             $scope.newTodo = {};
             $scope.addTodo = addTodo;
             $scope.deleteTodo = deleteTodo;
+            $scope.update = update;
+            $scope.edit = edit;
             getTodos();
             function deleteTodo(todo){
               TodoService.delete(todo)
@@ -16,6 +18,20 @@
                             //console.log(response);
                             getTodos();
                           });
+            }
+
+            function edit(todo){
+              console.log('editing...');
+              todo.edit = true;
+            }
+
+            function update(todo){
+              console.log('updating...');
+              todo.edit = false;
+              TodoService.update(todo)
+                        .then(function(response){
+                          getTodos();
+                        });
             }
 
             function addTodo(newTodo){
--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -19,10 +19,15 @@
           return $http.delete(`${baseURL}/${todo._id}`);
         }
 
+        function update(todo){
+          return $http.put(`${baseURL}/${todo._id}`, todo);
+        }
+
         return {
           getAll: getAll,
           create: create,
-          delete: deleteTodo
+          delete: deleteTodo,
+          update: update
         };
       }
 })()
